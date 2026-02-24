2962bd13070f492470943cdfd28bd53b4c8328a4
FLIPPED-RN <rnazarov142@gmail.com>
2026-02-24 12:37:03 +0200

    Добавьте файлы проекта.

---
+++ lab3-instrum-dev/Program.cs
@@ -0,0 +1,56 @@
+﻿Printer printer = new Printer(new LaserPrintStrategy());
+printer.Print("дарова");
+
+printer.SetStrategy(new InkjetPrintStrategy());
+printer.Print("фото.пнг");
+
+printer.SetStrategy(new ThreeDPrintStrategy());
+printer.Print("модель.stl");
+
+//-------------------------------------------
+interface IPrintStrategy
+{
+    void Print(string message);
+}
+
+class LaserPrintStrategy : IPrintStrategy
+{
+    public void Print(string message)
+    {
+        Console.WriteLine($"лазерная печать: {message}");
+    }
+}
+
+class InkjetPrintStrategy : IPrintStrategy
+{
+    public void Print(string message)
+    {
+        Console.WriteLine($"струйная печать: {message}");
+    }
+}
+
+class ThreeDPrintStrategy : IPrintStrategy
+{
+    public void Print(string message)
+    {
+        Console.WriteLine($"3D печать: {message}");
+    }
+}
+
+class Printer
+{
+    private IPrintStrategy _printStrategy;
+    public Printer(IPrintStrategy printStrategy)
+    {
+        _printStrategy = printStrategy;
+    }
+
+    public void SetStrategy(IPrintStrategy printStrategy)
+    {
+        _printStrategy = printStrategy;
+    }
+    public void Print(string message)
+    {
+        _printStrategy.Print(message);
+    }
+}
